a9dcc95a32079d1a40b22da29e2f81dd85c7a1a5
Beaver <joongsim22@gmail.com>
2017-03-28 17:52:19 +0900

    잘 올라가는지 봐보자.

--- app/src/main/java/test/test/MainActivity.java
+++ app/src/main/java/test/test/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String str = "잘 반영되는지 봐보자.";
     }
 }
